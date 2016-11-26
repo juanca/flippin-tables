bc7018dbdfa42d1e685c021725d09fcf59afdf18
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-26 15:18:08 -0800

    Pick up objects and stuff

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+  public GameObject player;
+
+  private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+    offset = transform.position - player.transform.position;
+	}
+	
+  // LateUpdate is called after all Update functions have been called
+	void LateUpdate () {
+    transform.position = player.transform.position + offset;
+	}
+}
